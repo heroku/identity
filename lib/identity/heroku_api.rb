71b5af168e2e5ad2fa5f0391e78af99ab80292cb
Brandur <brandur@mutelight.org>
2013-01-04 16:31:06 -0800

    Use URL-safe Base64 encoding

--- lib/identity/heroku_api.rb
+++ lib/identity/heroku_api.rb
@@ -3,7 +3,7 @@ require "base64"
 module Identity
   class HerokuAPI < Excon::Connection
     def initialize(options={})
-      authorization = Base64.encode64(
+      authorization = Base64.urlsafe_encode64(
         "#{options[:user] || ''}:#{options[:pass] || ''}")
       super(
         Config.heroku_api_url,
