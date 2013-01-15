7a202bffc678e5da01580e042b756e47d2113628
Brandur <brandur@mutelight.org>
2013-01-15 15:34:57 -0800

    Update general service stub code from API

--- test/service_stubs.rb
+++ test/service_stubs.rb
@@ -1,10 +1,11 @@
 require_relative "service_stubs/heroku_api_stub"
 
 def stub_service(uri, stub, &block)
-  uri = URI(uri)
-  uri.user, uri.password = nil, nil
+  uri = URI.parse(uri)
+  port = uri.port != uri.default_port ? ":#{uri.port}" : ""
   stub = block ? Sinatra.new(stub, &block) : stub
-  stub_request(:any, /^#{uri}\/.*$/).to_rack(stub)
+  stub_request(:any, /^#{uri.scheme}:\/\/(.*:.*@)?#{uri.host}#{port}\/.*$/).
+    to_rack(stub)
 end
 
 def stub_heroku_api(&block)
