ba3f666a4cb0f19788ce2097e9a7db93ea776fd4
Brandur <brandur@mutelight.org>
2013-02-14 12:32:50 -0800

    Clear strange Ruby coding style

--- lib/identity/main.rb
+++ lib/identity/main.rb
@@ -5,22 +5,24 @@ module Identity
 
     # general cookie storing information of the logged in user; should be
     # scoped to the app's specific domain
-    use Rack::Session::Cookie, domain: Config.cookie_domain,
-                               path: '/',
-                               expire_after: 2592000,
-                               secret: Config.secure_key,
-                               coder: FernetCookieCoder.new(Config.cookie_encryption_key),
-                               http_only: true
+    use Rack::Session::Cookie,
+      coder: FernetCookieCoder.new(Config.cookie_encryption_key),
+      domain: Config.cookie_domain,
+      http_only: true,
+      path: '/',
+      expire_after: 2592000,
+      secret: Config.secure_key
 
     # cookie with a domain scoped to all heroku domains, used to set a session
     # nonce value so that consumers can recognize when the logged in user has
     # changed
     if Config.heroku_cookie_domain
-      use Rack::Session::Cookie, domain: Config.heroku_cookie_domain,
-                                 expire_after: 2592000,
-                                 key: 'rack.session.heroku',
-                                 secret: Config.secure_key,
-                                 http_only: true
+      use Rack::Session::Cookie,
+        domain: Config.heroku_cookie_domain,
+        expire_after: 2592000,
+        http_only: true,
+        key: 'rack.session.heroku',
+        secret: Config.secure_key
     end
 
     use Rack::Csrf, skip: ["POST:/oauth/.*"]
