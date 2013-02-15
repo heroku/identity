3d0500d66806d16b29b074b8b8568169c9193289
Brandur <brandur@mutelight.org>
2013-02-15 08:38:40 -0800

    CHANGE STUFF TO FIND BUG

--- lib/identity/heroku_api.rb
+++ lib/identity/heroku_api.rb
@@ -7,12 +7,13 @@ module Identity
         "Accept" => "application/vnd.heroku+json; version=3"
       }.merge(options[:headers] || {})
       if options[:user] || options[:pass]
-        authorization = Base64.urlsafe_encode64(
-          "#{options[:user] || ''}:#{options[:pass] || ''}")
+        authorization = ["#{options[:user] || ''}:#{options[:pass] || ''}"].
+          pack('m').delete("\r\n")
         headers["Authorization"] = "Basic #{authorization}"
       elsif options[:authorization]
         headers["Authorization"] = options[:authorization]
       end
+      Slides.log :ACTION, headers
       super(Config.heroku_api_url, headers: headers,
         instrumentor: ExconInstrumentor.new(id: options[:request_id]))
     end
