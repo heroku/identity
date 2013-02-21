822f61a3541a57b478627d04c635f49cdae263e1
Brandur <brandur@mutelight.org>
2013-02-21 15:30:46 -0800

    Move FernetCookieCoder to Identity namespace

--- lib/identity/fernet_cookie_coder.rb
+++ lib/identity/fernet_cookie_coder.rb
@@ -1,31 +1,31 @@
 require "base64"
 require "rack/session/cookie"
 
-class FernetCookieCoder
-  attr_accessor :key
-
-  def initialize(key)
-    @key = key
-  end
-
-  def encode(raw)
-    data = Base64.urlsafe_encode64(Marshal.dump(raw))
-    Fernet.generate(key) do |generator|
-      # use Marshal instead of JSON to avoid trouble with string/symbol
-      # conversions
-      generator.data = { "session" => data }
+module Identity
+  class FernetCookieCoder
+    def initialize(*keys)
+      @keys = keys
     end
-  end
 
-  def decode(token)
-    verifier = Fernet.verifier(key, token)
-    verifier.enforce_ttl = false
-    verifier.verify_token(token)
-    raise "signature invalid" unless verifier.valid?
-    Marshal.load(Base64.urlsafe_decode64(verifier.data["session"]))
-  # fernet throws random exceptions :{ eat it for now
-  rescue Exception => e
-    Slides.log(:exception, class: e.class.name, message: e.message)
-    {}
+    def encode(raw)
+      data = Base64.urlsafe_encode64(Marshal.dump(raw))
+      Fernet.generate(@keys.first) do |generator|
+        # use Marshal instead of JSON to avoid trouble with string/symbol
+        # conversions
+        generator.data = { "session" => data }
+      end
+    end
+
+    def decode(token)
+      verifier = Fernet.verifier(@keys.first, token)
+      verifier.enforce_ttl = false
+      verifier.verify_token(token)
+      raise "signature invalid" unless verifier.valid?
+      Marshal.load(Base64.urlsafe_decode64(verifier.data["session"]))
+    # fernet throws random exceptions :{ eat it for now
+    rescue Exception => e
+      Slides.log(:exception, class: e.class.name, message: e.message)
+      {}
+    end
   end
 end
