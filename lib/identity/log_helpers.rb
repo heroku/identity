b978733e57f479700b933ee020e5fe0756d382f5
Brandur <brandur@mutelight.org>
2013-03-04 10:32:53 -0800

    Fix logging disconnect

--- lib/identity/log_helpers.rb
+++ lib/identity/log_helpers.rb
@@ -3,8 +3,7 @@ module Identity
     def log(action, data={}, &block)
       data.merge! app: "identity",
         id: request_ids ? request_ids.join(",") : nil
-      data += request_ids.map { |id| [:id, id] } if request_ids
-      Slides.log_array(action, data, &block)
+      Slides.log(action, data, &block)
     end
 
     def request_ids
