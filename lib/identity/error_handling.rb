f4cd9fa6e9da5df605f85587b423152e41a70188
Brandur <brandur@mutelight.org>
2013-03-15 16:48:27 -0600

    Gracefully handle 503s and communication errors with the API

--- lib/identity/error_handling.rb
+++ lib/identity/error_handling.rb
@@ -1,10 +1,25 @@
 module Identity
   module ErrorHandling
+    UNAVAILABLE_ERRORS = [
+      Excon::Errors::ServiceUnavailable,
+      Excon::Errors::SocketError,
+      Excon::Errors::Timeout,
+    ]
+
     def self.registered(app)
+      app.error *UNAVAILABLE_ERRORS do
+        e = env["sinatra.error"]
+        Identity.log(:exception, type: :unavailable,
+          class: e.class.name, message: e.message,
+          id: request.env["REQUEST_ID"], backtrace: e.backtrace.inspect)
+        slim :"errors/503", layout: :"layouts/classic"
+      end
+
       app.error do
         e = env["sinatra.error"]
         Airbrake.notify(e) if Config.airbrake_api_key
-        Identity.log(:exception, class: e.class.name, message: e.message,
+        Identity.log(:exception,
+          class: e.class.name, message: e.message,
           id: request.env["REQUEST_ID"], backtrace: e.backtrace.inspect)
         slim :"errors/500", layout: :"layouts/classic"
       end
