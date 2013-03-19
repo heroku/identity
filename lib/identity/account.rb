39f2c5e18b947a7ee3cb33d8fdb5cafe0af47888
Brandur <brandur@mutelight.org>
2013-03-18 18:10:48 -0600

    Correct `decode_error` method

--- lib/identity/account.rb
+++ lib/identity/account.rb
@@ -195,7 +195,7 @@ module Identity
       #   1. { "id":..., "message":... } (V3)
       #   2. { "error":... } (V2)
       #   3. [["password","is too short (minimum is 6 characters)"]] (V-Insane)
-      json = MultiJson.decode(e.response.body)
+      json = MultiJson.decode(body)
       !json.is_a?(Array) ?
         json["error"] || json["message"] :
         json.map { |e| e.join(" ") }.join("; ")
