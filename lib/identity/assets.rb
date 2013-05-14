3cb6bca46b96d58854ae03eb441d921c744d9552
Brandur <brandur@mutelight.org>
2013-05-14 08:49:08 -0600

    Compute content_type from Sprockets; use images/favicon.ico

--- lib/identity/assets.rb
+++ lib/identity/assets.rb
@@ -21,46 +21,42 @@ module Identity
     end
 
     get "/assets/:release/classic.css" do
-      content_type("text/css")
       respond_with_asset(@assets["classic.css"])
     end
 
     get "/assets/:release/classic.js" do
-      content_type("application/javascript")
       respond_with_asset(@assets["classic.js"])
     end
 
     get "/assets/:release/zen-backdrop.css" do
-      content_type("text/css")
       respond_with_asset(@assets["zen_backdrop.css"])
     end
 
     get "/assets/:release/zen-backdrop.js" do
-      content_type("application/javascript")
       respond_with_asset(@assets["zen_backdrop.js"])
+    end
+
+    get "/favicon.ico" do
+      respond_with_asset(@assets["favicon.ico"])
     end
 
     (%w{ico jpg png} + %w{eot svg ttf woff}).each do |format|
       get "/assets/*.#{format}" do |image|
         name = "#{image}.#{format}"
         if @assets[name]
-          content_type("image/#{format}")
-          respond_with_asset(@assets[name])
+          respond_with_asset(@assets[name], format)
         else
           404
         end
       end
     end
 
-    get "/favicon.ico" do
-      content_type("image/x-icon")
-      respond_with_asset(@assets["favicon.ico"])
-    end
-
     private
 
     def respond_with_asset(asset)
       cache_control(:public, max_age: 2592000)
+      #content_type(Rack::Mime.mime_type(File.extname(asset.pathname)))
+      content_type(asset.content_type)
       last_modified(asset.mtime.utc)
       asset
     end
