05149298e40b3c6b5b26d5b7d0bf57ce0706e1fc
Raul Murciano <raul@murciano.net>
2013-08-12 13:32:28 +0200

    Log oauth_dance_id if it's found as a cookie in the requests

--- lib/identity/log_helpers.rb
+++ lib/identity/log_helpers.rb
@@ -2,6 +2,7 @@ module Identity
   module LogHelpers
     def log(action, data={}, &block)
       data.merge! app: "identity", request_id: request_ids
+      data.merge! oauth_dance_id: @oauth_dance_id if @oauth_dance_id
       Slides.log(action, data, &block)
     end
 
