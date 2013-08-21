27e13de6865711718eb9bfa61ce4b9fb57d37d2c
Brandur <brandur@mutelight.org>
2013-08-21 09:53:05 -0700

    Add exception context to logging as well

--- lib/identity/error_handling.rb
+++ lib/identity/error_handling.rb
@@ -21,21 +21,21 @@ module Identity
 
       app.error do
         e = env["sinatra.error"]
-        Identity.log(
-          :exception,
-          class: e.class.name, message: e.message,
-          request_id: request.env["REQUEST_IDS"],
-          backtrace: e.backtrace.inspect
-        )
-        Airbrake.notify(e) if Config.airbrake_api_key
-        Honeybadger.notify(e, context: {
+        context = {
           method:          request.request_method,
           module:          self.class.name,
           request_id:      env["REQUEST_IDS"],
           route_signature: env["HTTP_X_ROUTE_SIGNATURE"],
           session_id:      @cookie ? @cookie.session_id : nil,
           user_id:         @cookie ? @cookie.user_id : nil,
-        }) if Config.honeybadger_api_key
+        }
+        Identity.log(:exception, {
+          class: e.class.name,
+          message: e.message,
+          backtrace: e.backtrace.inspect
+        }.merge(context))
+        Airbrake.notify(e) if Config.airbrake_api_key
+        Honeybadger.notify(e, context: context) if Config.honeybadger_api_key
         slim :"errors/500", layout: :"layouts/classic"
       end
     end
