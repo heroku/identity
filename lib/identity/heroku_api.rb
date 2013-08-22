96deed10b0108cd248d4bb38adcc8214966d8817
Brandur <brandur@mutelight.org>
2013-08-22 14:04:11 -0700

    Send application/json with version 3

--- lib/identity/heroku_api.rb
+++ lib/identity/heroku_api.rb
@@ -7,13 +7,20 @@ module Identity
       ip          = options[:ip] || raise("missing=ip")
       request_ids = options[:request_ids] || []
       version     = options[:version] || raise("missing=version")
+
       headers = {
         "Accept"          => "application/vnd.heroku+json; version=#{version}",
         # explicitly specify this or bodies will be interpreted as JSON
-        "Content-Type"    => "application/x-www-form-urlencoded",
         "Request-ID"      => request_ids,
         "X-Forwarded-For" => ip,
       }.merge!(headers)
+
+      if version == 2
+        headers.merge!("Content-Type" => "application/x-www-form-urlencoded")
+      else
+        headers.merge!("Content-Type" => "application/json")
+      end
+
       if options[:user] || options[:pass]
         authorization = ["#{options[:user] || ''}:#{options[:pass] || ''}"].
           pack('m').delete("\r\n")
@@ -21,6 +28,7 @@ module Identity
       elsif options[:authorization]
         headers["Authorization"] = options[:authorization]
       end
+
       super(Config.heroku_api_url, headers: headers, instrumentor:
         ExconInstrumentor.new(request_id: request_ids))
     end
