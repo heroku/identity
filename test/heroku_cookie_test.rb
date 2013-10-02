3bac5b2f5a926c8a4a67e8339f96961d357b7255
Brandur <brandur@mutelight.org>
2013-10-01 17:02:06 -0700

    Add test to check for cookie persistence

--- test/heroku_cookie_test.rb
+++ test/heroku_cookie_test.rb
@@ -20,7 +20,32 @@ describe Identity::HerokuCookie do
     assert_includes headers["Set-Cookie"], "heroku_session_nonce=;"
   end
 
+  it "persists the Heroku cookie through a standard request" do
+    cookie = cookies_for_nonce("1234")
+
+    app = middleware(-> (env) {
+      [200, {}, {}]
+    })
+    _, headers, _ = app.call({ "HTTP_COOKIE" => cookie })
+    assert_includes headers["Set-Cookie"], "heroku_session=1;"
+    assert_includes headers["Set-Cookie"], "heroku_session_nonce=1234;"
+  end
+
   private
+
+  def cookies_for_nonce(nonce)
+    app = middleware(-> (env) {
+      env["heroku.cookie"] = { "nonce" => nonce }
+      [200, {}, {}]
+    })
+    _, headers, _ = app.call({})
+
+    # Rack will separate multiple `Set-Cookie` headers with a newline; grab the
+    # key=value from all of these; then combine them back together with a
+    # semicolon for injection back in via `Cookie` on request
+    cookies = headers["Set-Cookie"].split("\n")
+    cookies.map { |c| c.split(";")[0] }.join(";")
+  end
 
   def middleware(app)
     Identity::HerokuCookie.new(
