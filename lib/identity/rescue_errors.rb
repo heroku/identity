8a31abdd56cf2b0b5c231f31f46e02cf90efc6ac
Mark Fine <mark.fine@gmail.com>
2014-01-08 21:10:21 -0500

    fix the middleware.

--- lib/identity/rescue_errors.rb
+++ lib/identity/rescue_errors.rb
@@ -1,11 +1,11 @@
 module Identity
   class RescueErrors
-    def initialize(app, opts={})
-      super(app, opts)
+    def initialize(app)
+      @app = app
     end
 
     def call(env)
-      super(env)
+      @app.call(env)
     rescue Exception => e
       Identity.log(:exception, class: e.class.name, message: e.message,
         backtrace: e.backtrace.inspect)
