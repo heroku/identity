f3e1a1bd448ccb8e97835d66d40f6bf73876dba9
Raul Murciano <raul@murciano.net>
2014-03-25 20:00:37 -0700

    Enclose JS password meter in its own function

--- assets/javascripts/classic/forms.js
+++ assets/javascripts/classic/forms.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  
+
   // -- add classes to form fields
   $('html body input[type="text"], html body input[type="password"]').addClass("text");
   $('input[type="submit"]').addClass("submit");
@@ -23,83 +23,88 @@ $(document).ready(function() {
 
   // ********************* PASSWORD METER
 
-  if ($('input#change_passwd').length > 0) var $button = $('input#change_passwd');
+  var applyPasswordMeter = function() {
+    var $button = $('input#change_passwd');
 
-  var passwordMessaging = function(value)
-  {    
-    var minLength  = value.length >= 8,
-        goodLength = value.length >= 12,
-        hasNumeric = value.match(/\d/),
-        hasAlpha   = value.match(/[a-z]/),
-        hasCapital = value.match(/[A-Z]/),
-        hasNonAlphaNumeric = value.match(/[^a-zA-Z0-9]/);
-        
-    var weak = minLength,
-        good = goodLength && hasAlpha && hasNumeric,
-        strong = goodLength && hasAlpha && hasNumeric && hasCapital && hasNonAlphaNumeric;
-
-    var $hint = $('span.hint'),
-        hints = {
-          strong:  'Strong password',
-          good:    'Good password',
-          weak:    'Weak password',
-          defaulty: 'minimum 8 characters letters,<br>numbers, and symbols'
-        },
-        passwordRating;
-
-    $hint.removeClass('strong good weak defaulty');
-
-    $button.removeAttr('disabled').removeClass('disabled');
-
-    if(strong)
-      passwordRating = 'strong';
-    else if(good)
-      passwordRating = 'good';
-    else if(weak)
-      passwordRating = 'weak';
-    else {
-      passwordRating = 'defaulty';
-      $button.attr('disabled','true').addClass('disabled');
-    }
-    
-    $hint.html(hints[passwordRating]).addClass(passwordRating);
-  };
-
-  $('#user_password').bind('keyup', function(){ passwordMessaging(this.value) });
-  $('#user_password_confirmation').bind('keyup', function(){
-    var password = $('#user_password').val(),
-        password_confirmation = $('#user_password_confirmation').val();
-
-    if(password && password === password_confirmation)
-      passwordMessaging(this.value);
-  });
-  $passwordFields = $('#user_password, #user_password_confirmation');
-
-  $passwordFields.bind('blur', function(){
-    var password = $('#user_password').val(),
-        password_confirmation = $('#user_password_confirmation').val();
-
-    if(password && password_confirmation && password !== password_confirmation)
+    var passwordMessaging = function(value)
     {
-      $('.new-password .hint')
-        .text('Passwords do not match')
-        .removeClass('weak')
-        .removeClass('good')
-        .removeClass('strong')
-        .addClass('bad-match');
-    }
-  });
+      var minLength  = value.length >= 8,
+          goodLength = value.length >= 12,
+          hasNumeric = value.match(/\d/),
+          hasAlpha   = value.match(/[a-z]/),
+          hasCapital = value.match(/[A-Z]/),
+          hasNonAlphaNumeric = value.match(/[^a-zA-Z0-9]/);
 
-  $passwordFields.bind('focus', function(){
-    if( $('.new-password .hint.bad-match').size() )
-    {
-      $('.new-password .hint').removeClass('bad-match')
-     
-      passwordMessaging('');
-    }
-  });
+      var weak = minLength,
+          good = goodLength && hasAlpha && hasNumeric,
+          strong = goodLength && hasAlpha && hasNumeric && hasCapital && hasNonAlphaNumeric;
 
-  if ($('#user_password').length > 0) passwordMessaging($('#user_password').val());
+      var $hint = $('span.hint'),
+          hints = {
+            strong:  'Strong password',
+            good:    'Good password',
+            weak:    'Weak password',
+            defaulty: 'minimum 8 characters letters,<br>numbers, and symbols'
+          },
+          passwordRating;
+
+      $hint.removeClass('strong good weak defaulty');
+
+      $button.removeAttr('disabled').removeClass('disabled');
+
+      if(strong)
+        passwordRating = 'strong';
+      else if(good)
+        passwordRating = 'good';
+      else if(weak)
+        passwordRating = 'weak';
+      else {
+        passwordRating = 'defaulty';
+        $button.attr('disabled','true').addClass('disabled');
+      }
+
+      $hint.html(hints[passwordRating]).addClass(passwordRating);
+    };
+
+    $('#user_password').bind('keyup', function(){ passwordMessaging(this.value) });
+    $('#user_password_confirmation').bind('keyup', function(){
+      var password = $('#user_password').val(),
+          password_confirmation = $('#user_password_confirmation').val();
+
+      if(password && password === password_confirmation)
+        passwordMessaging(this.value);
+    });
+    $passwordFields = $('#user_password, #user_password_confirmation');
+
+    $passwordFields.bind('blur', function(){
+      var password = $('#user_password').val(),
+          password_confirmation = $('#user_password_confirmation').val();
+
+      if(password && password_confirmation && password !== password_confirmation)
+      {
+        $('.new-password .hint')
+          .text('Passwords do not match')
+          .removeClass('weak')
+          .removeClass('good')
+          .removeClass('strong')
+          .addClass('bad-match');
+      }
+    });
+
+    $passwordFields.bind('focus', function(){
+      if( $('.new-password .hint.bad-match').size() )
+      {
+        $('.new-password .hint').removeClass('bad-match')
+
+        passwordMessaging('');
+      }
+    });
+
+    if ($('#user_password').length > 0) passwordMessaging($('#user_password').val());
+
+  }
+
+  if ($('input#change_passwd').length > 0) applyPasswordMeter()
 
 });
 
