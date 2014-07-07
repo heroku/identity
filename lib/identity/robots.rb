2fe6e218f4d2c52b034e61981c603e573dc8c39d
Brandur <brandur@mutelight.org>
2014-07-07 14:38:20 -0700

    Comment robots.txt

--- lib/identity/robots.rb
+++ lib/identity/robots.rb
@@ -5,6 +5,8 @@ module Identity
     get "/robots.txt" do
       content_type :text
       <<-eos
+# Won't actually prevent these folders from appearing in a search engine's
+# index, but isn't really harmful either.
 User-agent: *
 Disallow: /account/email/confirm/
 Disallow: /account/password/reset/
