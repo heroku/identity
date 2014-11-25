4aaf836f9310a2d087ee881e878a131b31efadd9
Raul Murciano <raul@murciano.net>
2014-11-24 17:45:14 -0800

    Remove references to old layouts in /design

--- lib/identity/design.rb
+++ lib/identity/design.rb
@@ -1,3 +1,7 @@
+# To test a new design layout called `foo`:
+# - add a view in `views/layouts/foo.slim`
+# - replicate the design links adding `?layout=foo`
+
 module Identity
   class Design < Sinatra::Base
     register Sinatra::Namespace
@@ -8,44 +12,44 @@ module Identity
 
     before do
       flash[:error] = nil
-      @layout = :"layouts/purple" if params[:purple]
+      @layout = params[:layout] ? :"layouts/#{params[:layout]}" : :"layouts/purple"
     end
 
     namespace "/design" do
       get "/login" do
-        slim :"login", layout: @layout || :"layouts/zen_backdrop"
+        slim :"login", layout: @layout
       end
 
       get "/login-flash" do
         flash[:error] = "There was a problem with your login."
-        slim :"login", layout: @layout || :"layouts/zen_backdrop"
+        slim :"login", layout: @layout
       end
 
       get "/two-factor" do
-        slim :"two-factor", layout: @layout || :"layouts/zen_backdrop"
+        slim :"two-factor", layout: @layout
       end
 
       get "/errors/:error" do # 404, 500, 503
-        slim :"errors/#{params['error']}", layout: @layout || :"layouts/classic"
+        slim :"errors/#{params['error']}", layout: @layout
       end
 
       get "/authorize" do
         @client = { "name" => "An example app"}
-        slim :"clients/authorize", layout: @layout || :"layouts/zen_backdrop"
+        slim :"clients/authorize", layout: @layout
       end
 
       namespace "/password" do
         get "/finish-reset" do
           @user = { "email" => "user@example.com"}
-          slim :"account/password/finish_reset", layout: @layout || :"layouts/zen_backdrop"
+          slim :"account/password/finish_reset", layout: @layout
         end
 
         get "/not-found" do
-          slim :"account/password/not_found", layout: @layout || :"layouts/zen_backdrop"
+          slim :"account/password/not_found", layout: @layout
         end
 
         get "/reset" do
-          slim :"account/password/reset", layout: @layout || :"layouts/zen_backdrop"
+          slim :"account/password/reset", layout: @layout
         end
       end
 
