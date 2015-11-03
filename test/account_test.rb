0f53fcd86506d39daea811696349045dac787b80
Doug McInnes <doug@dougmcinnes.com>
2015-11-03 10:27:48 -0800

    double quotes

--- test/account_test.rb
+++ test/account_test.rb
@@ -86,7 +86,7 @@ describe Identity::Account do
       stub_heroku_api
       post "/account/password/reset", email: "kerry@heroku.com"
       assert_equal 200, last_response.status
-      refute last_request.env['x-rack.flash']['notice'].nil?
+      refute last_request.env["x-rack.flash"]["notice"].nil?
     end
 
     it "redirects when the api responded with an error" do
