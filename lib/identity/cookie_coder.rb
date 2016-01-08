e849bf8e79546f07980caa9b08d0994368e4d162
Jingwen Owen Ou <jingweno@gmail.com>
2016-01-08 10:27:10 -0800

    Add `CookieCoder`

---
+++ lib/identity/cookie_coder.rb
@@ -0,0 +1,53 @@
+require "base64"
+
+module Identity
+  # CookieCoder encodes and decodes payload before setting it to cookie
+  # Different from FernetCookieCoder, it converts Ruby objects to JSON
+  # format before encrypting using HMAC with Fernet. The encrypted cookie
+  # is meant to consumed by other programming languages other than Ruby.
+  class CookieCoder
+    def initialize(*keys)
+      @keys = keys.compact
+    end
+
+    def encode(payload)
+      Fernet.generate(@keys.first) do |generator|
+        generator.data = { "data" => payload }
+      end
+    end
+
+    def decode(cipher)
+      return {} if cipher == nil
+
+      plain = nil
+      @keys.each do |key|
+        begin
+          plain = decode_with_key(cipher, key)
+        rescue OpenSSL::Cipher::CipherError
+        end
+
+        break if plain
+      end
+
+      raise "no valid encryption key for cipher" unless plain
+
+      plain
+    rescue Exception => e
+      Identity.log(:exception, class: e.class.name, message: e.message,
+                   fernet: true, backtrace: e.backtrace.inspect)
+      {}
+    end
+
+    private
+
+    def decode_with_key(cipher, key)
+      verifier = Fernet.verifier(key, cipher)
+      verifier.enforce_ttl = false
+      verifier.verify_token(cipher)
+
+      raise "cipher invalid" unless verifier.valid?
+
+      verifier.data["data"]
+    end
+  end
+end
---
+++ test/cookie_coder_test.rb
@@ -0,0 +1,29 @@
+require_relative "test_helper"
+
+describe Identity::CookieCoder do
+  it "encryptes a cookie, then descripts a cookie" do
+    data = { "user" => { "id" => "1234", "email" => "example@herou.com", "full_name" => "Full Name" }}
+    coder = Identity::CookieCoder.new("my-key" * 20)
+    cipher = coder.encode(data)
+
+    assert_equal data, coder.decode(cipher)
+  end
+
+  it "encrypts with the first given key" do
+    data = { "user" => { "id" => "1234", "email" => "example@herou.com", "full_name" => "Full Name" }}
+    coder = Identity::CookieCoder.new("my-key" * 20)
+    coder.encode(data)
+    coder = Identity::CookieCoder.new("my-new-key" * 20, "my-key" * 20)
+
+    refute_equal data, coder.encode(data)
+  end
+
+  it "enables graceful encryption key rotation" do
+    data = { "user" => { "id" => "1234", "email" => "example@herou.com", "full_name" => "Full Name" }}
+    coder = Identity::CookieCoder.new("my-key" * 20)
+    cipher = coder.encode(data)
+    coder = Identity::CookieCoder.new("my-new-key" * 20, "my-key" * 20)
+
+    assert_equal data, coder.decode(cipher)
+  end
+end
